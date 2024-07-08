5e392186812834f090c388826cb311d8628b9bf3
meumoc <113320440+meumocdev@users.noreply.github.com>
2024-07-08 09:17:04 +0700

    Update Case Study #6: Clique Bait.sql

--- Case Study #6: Clique Bait.sql
+++ Case Study #6: Clique Bait.sql
@@ -78,3 +78,59 @@ GROUP BY p.product_category
 ORDER BY product DESC 
 LIMIT 3;
 
+-- B. Product Funnel Analysis
+WITH purchase_events AS (
+    SELECT DISTINCT visit_id
+    FROM events
+    WHERE event_type = 3
+),
+product_info AS (
+    SELECT 
+        ph.page_name AS products_name,
+        ph.product_category,
+        SUM(CASE WHEN e.event_type = 1 THEN 1 ELSE 0 END) AS viewed,
+        SUM(CASE WHEN e.event_type = 2 THEN 1 ELSE 0 END) AS cart_adds,
+        SUM(CASE WHEN e.event_type = 2 AND pe.visit_id IS NOT NULL THEN 1 ELSE 0 END) AS purchases,
+        SUM(CASE WHEN e.event_type = 2 AND pe.visit_id IS NULL THEN 1 ELSE 0 END) AS abandoned
+    FROM events e
+    JOIN page_hierarchy ph ON e.page_id = ph.page_id
+    LEFT JOIN purchase_events pe ON e.visit_id = pe.visit_id
+    WHERE ph.product_id IS NOT NULL
+    GROUP BY ph.page_name, ph.product_category
+)
+-- 1. Which product had the most views, cart adds and purchases?
+SELECT 
+    products_name, 
+    product_category, 
+    viewed + cart_adds + purchases AS most_views_cart_adds_purchases
+FROM product_info
+ORDER BY most_views_cart_adds_purchases DESC
+LIMIT 1
+
+-- 2. Which product was most likely to be abandoned?
+SELECT 
+    products_name, 
+    product_category, 
+    abandoned
+FROM product_info
+ORDER BY abandoned
+LIMIT 1 
+
+-- 3. Which product had the highest view to purchase percentage? 
+SELECT 
+    product_name, 
+    product_category, 
+    ROUND(100.0 * purchases / viewed, 2) AS purchase_per_view_percentage
+FROM product_info
+ORDER BY purchase_per_view_percentage DESC
+	
+-- 4. What is the average conversion rate from view to cart add?
+SELECT 
+    AVG(CASE WHEN viewed = 0 THEN 0 ELSE (cart_adds / viewed) END) AS avg_conversion_rate
+FROM product_info
+	
+-- 5. What is the average conversion rate from cart add to purchase? 
+ SELECT 
+    ROUND(100.0 * AVG(cart_adds / viewed), 2) AS avg_view_to_cart_add_conversion,
+    ROUND(100.0 * AVG(purchases / cart_adds), 2) AS avg_cart_add_to_purchases_conversion_rate
+FROM product_info
